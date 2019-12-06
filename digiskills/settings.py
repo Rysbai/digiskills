63b1f3c56c4b0c47c65eb38066c1b0614a51bb2c
rysbai <rysbai.coder@gmail.com>
2019-12-06 21:23:32 +0600

    Changed debug

--- digiskills/settings.py
+++ digiskills/settings.py
@@ -3,7 +3,7 @@ from decouple import config, Csv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = (config('DEBUG') == 'True')
 
 ALLOWED_HOSTS = '*'
 
