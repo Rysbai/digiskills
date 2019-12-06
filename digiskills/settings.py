cb4b5e7d6513b566d1bcff2f25724ec836833a3b
rysbai <rysbai.coder@gmail.com>
2019-12-06 21:45:02 +0600

    Allowed hosts changed

--- digiskills/settings.py
+++ digiskills/settings.py
@@ -65,7 +65,7 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
