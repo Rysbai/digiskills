d4275bad6bd8eae79d6b4cecf7846cf5258851f6
rysbai <rysbai.coder@gmail.com>
2019-12-18 21:39:09 +0600

    Added absolute path to images in serializers

---
+++ utils/get_absolute_url.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+PROTOCOL = 'https' if settings.SSL_SECURED else 'http'
+
+
+def get_absolute_url(relative_path):
+    return '%s://%s:%s%s' % (PROTOCOL, settings.HOST_NAME, settings.PORT, relative_path)
