89e9b7642c989d2b00cbd28282e0f9b9c0a454f8
Rysbai <rysbai.coder@gmail.com>
2020-01-12 22:53:06 +0600

    Removed csrf middleware, changed send_comment_to_admins, removed django_heroku and psycorg2 from requirentments

--- comment/utils.py
+++ comment/utils.py
@@ -5,19 +5,20 @@ from django.contrib.auth.models import User
 
 
 def send_comment_to_admin_email(name: str, phone: str, text: str, *args, **kwargs):
-    admin = User.objects.filter(~Q(email=''), is_superuser=True).first()
+    admins = User.objects.filter(~Q(email=''), is_superuser=True)
     subject = 'Комментарий к проекту digiskills от {}'.format(name)
     text = 'Комментарий от {}\n'.format(name) + \
            'Номер телефона: {}\n'.format(phone) + \
            text
 
-    try:
-        send_mail(
-            subject,
-            text,
-            settings.EMAIL_HOST_USER,
-            [admin.email],
-            fail_silently=False,
-        )
-    except Exception as exception:
-        pass
+    for admin in admins:
+        try:
+            send_mail(
+                subject,
+                text,
+                settings.EMAIL_HOST_USER,
+                [admin.email],
+                fail_silently=False,
+            )
+        except Exception as exception:
+            pass
