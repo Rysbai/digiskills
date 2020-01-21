1dbb7db61341f00fc63d5708c0b345b0a0bf688b
Rysbai <rysbai.coder@gmail.com>
2020-01-21 22:11:30 +0600

    Fixed ssl_renew script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -2,5 +2,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/digit/digiskills/
-$COMPOSE -f docker-compose.yml -f docker-compose-prod.yml run certbot renew --dry-run && $COMPOSE docker-compose.yml -f docker-compose-prod.yml kill -s SIGHUP nginx
+$COMPOSE -f docker-compose.yml -f docker-compose-prod.yml run certbot renew && $COMPOSE docker-compose.yml -f docker-compose-prod.yml kill -s SIGHUP nginx
 $DOCKER system prune -af
